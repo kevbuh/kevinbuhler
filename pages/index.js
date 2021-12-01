5b798e6007b8b3062259bbf1ae2effd17f9faa70
Kevin Buhler <kevinbuhlerr@gmail.com>
2021-12-01 02:05:08 -0700

    changed to my information on main page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
+// import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -13,25 +13,25 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+            <a href="https://kevinbuhler.vercel.app/">Kevin Buhler</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          {/* Building the future{' '} */}
+          <code className={styles.code}>building the future</code>
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="https://nextnote.io" className={styles.card}>
+            <h2>NextNote &rarr;</h2>
+            <p>Intelligent Note Taking</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          <a href="https://eyecandy.vercel.app/" className={styles.card}>
+            <h2>Eye Candy &rarr;</h2>
+            <p>Vote on your favorite looking movies or TV shows!</p>
           </a>
-
+{/* 
           <a
             href="https://github.com/vercel/next.js/tree/master/examples"
             className={styles.card}
@@ -48,11 +48,11 @@ export default function Home() {
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
-          </a>
+          </a> */}
         </div>
       </main>
 
-      <footer className={styles.footer}>
+      {/* <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
@@ -63,7 +63,7 @@ export default function Home() {
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
-      </footer>
+      </footer> */}
     </div>
   )
 }
