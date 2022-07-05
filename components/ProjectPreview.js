c723118112d6a44e7504c3fec41be8ba081885d0
Kevin Buhler <kevinbuhlerr@gmail.com>
2022-07-05 15:20:00 -0600

    finetune the images on front page

--- components/ProjectPreview.js
+++ components/ProjectPreview.js
@@ -3,14 +3,11 @@ function ProjectPreview({ title, caption, link, image }) {
     <div className="w-3/4 mx-auto mt-20">
       <div className="flex flex-row flex-wrap justify-between lg:flex-row-reverse mx-auto mt-20">
         <div className="sm:h-1/2">
-          <img
-            src={image}
-            className="max-w-sm rounded-lg shadow-xl mb-5 sm:h-1/2"
-          />
+          <img src={image} className="max-w-xs rounded-lg shadow-xl mb-5 " />
         </div>
         <div className="flex flex-col my-auto">
-          <p className="text-5xl font-semibold">{title}</p>
-          <p className="py-6 max-w-md">{caption}</p>
+          <p className="text-5xl font-semibold max-w-sm">{title}</p>
+          <p className="py-6 max-w-sm">{caption}</p>
 
           <a className="btn btn-outline" href={link}>
             More Info &rarr;
